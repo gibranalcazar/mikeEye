e9c3b585aa35c1086f5e1b259ca2a1047a17a58e
GN-AM <gibran@live.com>
2022-09-01 20:10:37 -0500

    S1

--- Eyes.js
+++ Eyes.js
@@ -5,7 +5,7 @@ document.onmousemove = (event) => {
   var x = (event.clientX * 100) / window.innerWidth + "%";
   var y = (event.clientY * 100) / window.innerHeight + "%";
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 1; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
     balls[i].style.transform = "translate(-" + x + ",-" + y + ")";
